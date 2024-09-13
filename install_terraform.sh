145daa5770bb9465df1cb6523f94566aa5e9c14f
kumarezzz <kumareshm44@gmail.com>
2024-09-13 11:41:57 +0530

    sudo permission given

--- install_terraform.sh
+++ install_terraform.sh
@@ -6,7 +6,7 @@ wget https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_linux_amd64.
 # Unzip and install
 unzip terraform_1.5.7_linux_amd64.zip
 chmod +x terraform
-mv terraform /usr/local/bin/
+sudo mv terraform /usr/local/bin/
 
 # Clean up
 rm terraform_1.5.7_linux_amd64.zip
